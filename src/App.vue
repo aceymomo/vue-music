1450a9478b2e66fc58a07e09d8e3dee281a7f149
aceymomo <kobezhang333@vip.qq.com>
2017-12-13 16:26:38 +0800

    The vue-music project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus">
+@import './common/stylus/variable'
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  font-size:$font-size-small-s
+</style>
